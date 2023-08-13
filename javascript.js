b374c6ddb16b548e4e427076ee8bb64b6f304dce
NOSTAlGiEAZ <nostalgieas@mail.ru>
2023-08-13 19:10:41 +0600

    Finished logic of the game
    Added scores for player and computer, changed return value for playRound() function.Implemented new function game(), RSP game will last until someone gets 3 points

--- javascript.js
+++ javascript.js
@@ -1,27 +1,43 @@
+const score = {
+  player: 0,
+  computer: 0
+};
+
 function getRandom(max=1){
   return (Math.random() * max);
 }
 
 function getComputerChoice() {
-  let randomNumber = getRandom();
-  let computerChoice = randomNumber < (1 / 3) ? 'rock' :
+  const randomNumber = getRandom();
+  const computerChoice = randomNumber < (1 / 3) ? 'rock' :
     randomNumber <= (2 / 3) ? 'scissors' : 'paper';
   return computerChoice;
 }
 
 function playRound(playerChoice, computerChoice) {
-  let result = playerChoice == computerChoice 
-    ? 'Tie'
+  const result = playerChoice == computerChoice 
+    ? `Tie. Score: ${++score.player}-${++score.computer}`
     : (playerChoice == 'rock' && computerChoice == 'scissors' ||
        playerChoice == 'scissors' && computerChoice == 'paper' ||
        playerChoice == 'paper' && computerChoice == 'rock')
-    ? `You Win! ${playerChoice} beats ${computerChoice}`
-    : `You Lose! ${computerChoice} beats ${playerChoice}`;
+    ? `You Win! ${playerChoice} beats ${computerChoice}. Score: ${++score.player}-${score.computer}`
+    : `You Lose! ${computerChoice} beats ${playerChoice}. Score: ${score.player}-${++score.computer}`;
   return result;
 }
 
-const computerChoice = getComputerChoice();
-const playerChoice = prompt("You choice: ").trim().toLowerCase();
-console.log(computerChoice);
-console.log(playRound(playerChoice, computerChoice));
+function game() {
+  while (score.computer < 3 && score.player < 3) {
+    const playerChoice = prompt('Your choice: ').trim().toLowerCase();
+    const computerChoice = getComputerChoice();
+    alert(playRound(playerChoice, computerChoice));
+  }
+  const result = score.player == score.computer
+    ? 'That\'s a Tie! What a DRAMA.'
+    : score.computer < score.player
+    ? 'Huh, you won... You are just being lucky :c'
+    : 'You LOST! Get better FOOL!';
+  alert(result);
+}
+
+game();
 
