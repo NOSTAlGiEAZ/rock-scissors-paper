8a0338e072f67b849819de8a054bf6a3a013a019
NOSTAlGiEAZ <nostalgieas@mail.ru>
2023-08-13 20:30:20 +0600

    Improved readability, added input validation

--- javascript.js
+++ javascript.js
@@ -3,23 +3,29 @@ const score = {
   computer: 0
 };
 
+const CHOICES = ['rock', 'scissors', 'paper'];
+const WINNING_COMBINATIONS = {
+  'rock': 'scissors', 
+  'scissors': 'paper', 
+  'paper': 'rock'
+};
+
 function getRandom(max=1){
   return (Math.random() * max);
 }
 
 function getComputerChoice() {
   const randomNumber = getRandom();
-  const computerChoice = randomNumber < (1 / 3) ? 'rock' :
-    randomNumber <= (2 / 3) ? 'scissors' : 'paper';
+  const computerChoice = randomNumber < (1 / 3) 
+    ? 'rock' : randomNumber <= (2 / 3) 
+    ? 'scissors' : 'paper';
   return computerChoice;
 }
 
 function playRound(playerChoice, computerChoice) {
   const result = playerChoice == computerChoice 
     ? `Tie. Score: ${++score.player}-${++score.computer}`
-    : (playerChoice == 'rock' && computerChoice == 'scissors' ||
-       playerChoice == 'scissors' && computerChoice == 'paper' ||
-       playerChoice == 'paper' && computerChoice == 'rock')
+    : WINNING_COMBINATIONS[playerChoice] == computerChoice
     ? `You Win! ${playerChoice} beats ${computerChoice}. Score: ${++score.player}-${score.computer}`
     : `You Lose! ${computerChoice} beats ${playerChoice}. Score: ${score.player}-${++score.computer}`;
   return result;
@@ -28,8 +34,12 @@ function playRound(playerChoice, computerChoice) {
 function game() {
   while (score.computer < 3 && score.player < 3) {
     const playerChoice = prompt('Your choice: ').trim().toLowerCase();
-    const computerChoice = getComputerChoice();
-    alert(playRound(playerChoice, computerChoice));
+    if (CHOICES.includes(playerChoice)) {
+      const computerChoice = getComputerChoice();
+      alert(playRound(playerChoice, computerChoice));
+    } else {
+      alert(`Can you type PROPERLY?? Choose between: ${CHOICES}`)
+    }
   }
   const result = score.player == score.computer
     ? 'That\'s a Tie! What a DRAMA.'
